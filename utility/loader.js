48256829c5c50638237a3443146e018554ced903
Davide Scarsi <davidescarsi93@gmail.com>
2026-02-26 15:25:30 +0100

    loader updated

--- utility/loader.js
+++ utility/loader.js
@@ -1,6 +1,5 @@
 // -----MAIN -----//
-window.__ETASS_BRANCH = 'main';
-fetch('https://raw.githubusercontent.com/Davide-Scarsi/Anonymous-autobot/' + window.__ETASS_BRANCH + '/Anonymous-autobot.js?t=' + Date.now())
+fetch('https://raw.githubusercontent.com/Davide-Scarsi/Anonymous-autobot/main/Anonymous-autobot.js?t=' + Date.now())
   .then(r => r.text())
   .then(code => eval(code))
   .catch(e => console.error('[Loader] Errore:', e));
